b2146abca92fdad7f445901db57238a0654ae279
Mack Hasz <mack@hasz.email>
2018-12-30 10:50:40 -0500

    basic search

--- app.rb
+++ app.rb
@@ -2,5 +2,17 @@ require "sinatra"
 require "sinatra/reloader" if development?
 
 get '/' do
-    'Hello world!!'
+    send_file File.join(settings.public_folder, 'index.html')
+end
+
+get '/search' do
+    query = URI.escape(params[:q])
+    
+    redirect "/!/#{query}"
+end
+
+get '/!/:query' do
+    query = params[:query]
+
+    erb :query, :locals => {:query => query}
 end
